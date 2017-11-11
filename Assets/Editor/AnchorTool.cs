77d7a71b19aa19125e1e2d09a4a2d82c917946c0
George Chew <hanged94@gmail.com>
2017-11-12 02:44:13 +0800

    Added login screen Added db

---
+++ Assets/Editor/AnchorTool.cs
@@ -0,0 +1,44 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class uGUITools : MonoBehaviour
+{
+    [MenuItem("uGUI/Anchors to Corners %[")]
+    static void AnchorsToCorners()
+    {
+        var objs = Selection.gameObjects;
+
+        foreach (var obj in objs)
+        {
+            RectTransform t = obj.GetComponent<RectTransform>();
+            RectTransform pt = obj.transform.parent.GetComponent<RectTransform>();
+
+            if (t == null || pt == null) continue;
+
+            Vector2 newAnchorsMin = new Vector2(t.anchorMin.x + t.offsetMin.x / pt.rect.width,
+                                                t.anchorMin.y + t.offsetMin.y / pt.rect.height);
+            Vector2 newAnchorsMax = new Vector2(t.anchorMax.x + t.offsetMax.x / pt.rect.width,
+                                                t.anchorMax.y + t.offsetMax.y / pt.rect.height);
+
+            t.anchorMin = newAnchorsMin;
+            t.anchorMax = newAnchorsMax;
+            t.offsetMin = t.offsetMax = new Vector2(0, 0);
+        }
+    }
+
+
+    [MenuItem("uGUI/Corners to Anchors %]")]
+    static void CornersToAnchors()
+    {
+        var objs = Selection.gameObjects;
+
+        foreach (var obj in objs)
+        {
+            RectTransform t = obj.GetComponent<RectTransform>();
+           
+            if (t == null) continue;
+            
+            t.offsetMin = t.offsetMax = new Vector2(0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class Login : MonoBehaviour
+{
+	[SerializeField]
+	Text loginErrorText = null;
+	string defaultError = "Incorrect username or password.";
+	string noUsernameError = "Please enter username.";
+	string noPasswordError = "Please enter password.";
+
+	public string Username { get; set; }
+	public string Password { get; set; }
+	public string Role;
+
+	SqliteConnection db = null;
+
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+
+		if (!File.Exists("db.sqlite")) Debug.LogError("DB not found!");
+		db = new SqliteConnection("Data Source=db.sqlite; Version=3");
+		db.Open();
+
+		loginErrorText.gameObject.SetActive(false);
+	}
+
+	void Update()
+	{
+	}
+
+	public void OnClickLogin()
+	{
+		loginErrorText.gameObject.SetActive(true);
+		loginErrorText.text = defaultError;
+
+		if (Username == null || Username == "")
+		{
+			loginErrorText.text = noUsernameError;
+			return;
+		}
+		else if (Password == null || Password == "")
+		{
+			loginErrorText.text = noPasswordError;
+			return;
+		}
+
+		try
+		{
+			using (SqliteCommand command = new SqliteCommand(db))
+			{
+				command.CommandText =
+					"SELECT role FROM users " +
+					"WHERE users.username = \"" + Username + "\" " +
+					"AND users.password = \"" + Password + "\"";
+
+				using (SqliteDataReader reader = command.ExecuteReader())
+				{
+					while (reader.Read())
+						Role = reader["role"].ToString();
+				}
+			}
+		}
+		catch
+		{
+		}
+
+		if (Role != "") loginErrorText.gameObject.SetActive(false);
+	}
+}
