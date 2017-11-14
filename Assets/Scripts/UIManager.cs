c6f25d9483ecb3ceb4aae699fb44db43ad35d02f
George Chew <hanged94@gmail.com>
2017-11-14 16:00:47 +0800

    Added stuff to work hours page

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,21 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
 	[SerializeField]
-	GameObject TopMenu, MainPage, HoursPage, LeaveRequestPage, EmployeePage, LeaveApprovalPage;
+	List<GameObject> pages = new List<GameObject>();
+
+	[SerializeField]
+	GameObject TopMenu, MainPage;
+
+	[SerializeField]
+	Button backButton;
 
 	// Use this for initialization
 	void Start()
 	{
-
+		foreach (GameObject p in pages) p.SetActive(false);
+		MainPage.SetActive(true);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
 
+	}
+
+	public void OpenPage(GameObject page)
+	{
+		foreach (GameObject p in pages) p.SetActive(p == page);
+		MainPage.SetActive(page == MainPage);
+		backButton.gameObject.SetActive(page != MainPage);
 	}
 }
